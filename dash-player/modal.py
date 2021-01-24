1b9d9fc1c93b0fae0c2f89b84d7848da3d8b6a62
Artemis Krd <artemis.ioanna@gmail.com>
2021-01-24 21:34:27 +0100

    * Adds order by functionality * Fixes: Table overview for two videos * Display current frame number for pixel videos * Move groupby buttons out of datatable
    Signed-off-by: Artemis Krd <artemis.ioanna@gmail.com>

--- dash-player/modal.py
+++ dash-player/modal.py
@@ -24,25 +24,38 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from datatable import render_datatable
+from dash.dependencies import Input, Output, State, MATCH
+from app import app
 
-def modal(df_angles, frame_no):
+def modal(df_angles, frame_no, index=1):
     return html.Div(
         [
-            dbc.Button("Table overview", id="open-centered"),
+            dbc.Button("Table overview", id={'type': 'modal-button-open', 'index': index}),
             dbc.Modal(
                 [
                     dbc.ModalHeader("Motion Rug (Video #1)"),
                     dbc.ModalBody(render_datatable(df_angles, frame_no, fullsize='true')),
                     dbc.ModalFooter(
                         dbc.Button(
-                            "Close", id="close-centered", className="ml-auto"
+                            "Close", id={'type': 'modal-button-close', 'index': index}, className="ml-auto"
                         )
                     ),
                 ],
-                id="modal-centered",
+                id={'type': 'modal', 'index': index},
                 centered=True,
                 size="xl",
                 style={"max-width": "90vw"}
             ),
         ]
     )
+
+@app.callback(
+    Output({'type': 'modal', 'index': MATCH}, "is_open"),
+    [Input({'type': 'modal-button-open', 'index': MATCH}, "n_clicks"),
+     Input({'type': 'modal-button-close', 'index': MATCH}, "n_clicks")],
+    [State({'type': 'modal', 'index': MATCH}, "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
